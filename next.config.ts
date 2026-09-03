bd47182dba5e84b9342f716ee7e3ec48fe70918c
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2026-09-03 17:27:12 +0530

    fix: disable image optimization (unoptimized) to fix 400 errors from sharp on server

--- next.config.ts
+++ next.config.ts
@@ -3,37 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   images: {
-    qualities: [75, 80, 90],
-    dangerouslyAllowLocalIP: true,
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8000",
-        pathname: "/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "admin.adcbind.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "adcb.janamithrasociety.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "http",
-        hostname: "adcb.janamithrasociety.com",
-        pathname: "/**",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
